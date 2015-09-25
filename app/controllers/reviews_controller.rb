93f7b89fc38b98fa8b676275d787d60f63992c5b
下向和紀 <simo@simo3.local>
2015-09-25 14:19:05 +0900

    reviews_controllerのnew、create作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  def new
+    @shop = Shop.find(params[:shop_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(
+      shop_id: params[:shop_id],
+      user_id: current_user.id,
+      comment: review_params["comment"]
+      )
+    if @review.save
+      redirect_to shop_url(@review.shop)
+    else
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:comment)
+  end
 end
