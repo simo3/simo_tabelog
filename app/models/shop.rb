9bb1e26794a9fbdedb440fcfb361bbb9a80d7243
下向和紀 <mail4simo@icloud.com>
2015-09-29 21:54:12 +0900

    shop.rbにクラスメソッドave_rate定義→index.html.erで呼び出し

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,13 @@
 class Shop < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+  def ave_rate
+   average = self.reviews.average(:rate)
+   if average == nil
+     return 0
+   else
+     return average
+   end
+ end
 end
